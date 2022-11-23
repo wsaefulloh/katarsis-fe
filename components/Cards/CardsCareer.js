b07872f32d70e54de6d7e73fd7c374a01ebf1fa0
Wahyu Saefulloh <saefuuloh@gmail.com>
2022-11-23 14:05:21 +0700

    add

--- components/Cards/CardsCareer.js
+++ components/Cards/CardsCareer.js
@@ -42,284 +42,148 @@ function CardsCareer(props) {
 
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
 
-  if (last == true) {
-    return (
-      <>
-        <div>
-          <div style={{ backgroundColor: "#000000", height: "1px" }}></div>
-          <div
-            className="py-4"
-            style={{
-              width: "100%",
-              // borderTop: "2px solid #000000",
-            }}
-          >
-            <Container>
-              <div
-                className="d-flex align-items-center justify-content-between"
-                {...getToggleProps()}
-              >
-                {isExpanded ? (
-                  <>
-                    <div>
-                      <h2
-                        style={{
-                          color: "#000000",
-                          fontStyle: "bold",
-                        }}
-                      >
-                        {`${title_departement}`}
-                      </h2>
-                      <div
-                        style={{ color: "#7B7B7B" }}
-                      >{`${length} OPEN ROLES`}</div>
-                    </div>
-                    <div>
-                      <img
-                        src={require("assets/img/icons/common/up_vector.png")}
-                      />
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div>
-                      <h2
-                        style={{
-                          color: "#000000",
-                          fontStyle: "bold",
-                        }}
-                      >
-                        {`${title_departement}`}
-                      </h2>
-                      <div
-                        style={{ color: "#7B7B7B" }}
-                      >{`${length} OPEN ROLES`}</div>
-                    </div>
-                    <div>
-                      <img
-                        src={require("assets/img/icons/common/down_vector.png")}
-                      />
-                    </div>
-                  </>
-                )}
-              </div>
-              <div className="mt-4" {...getCollapseProps()}>
-                <div>{`${description_departement}`}</div>
-                <div className="mt-4">
-                  {job.map((val, index) => {
-                    if (index % 2 == 0) {
-                      return (
-                        <div
-                          style={{ backgroundColor: "#D9D9D9CC" }}
-                          className="p-3 d-flex align-items-center"
-                        >
-                          <Col>{`${val.job_title}`}</Col>
-                          <Col className="text-center">{`${val.type_job}`}</Col>
-                          <Col className="text-center">{`${val.city}`}</Col>
-                          <Col className="text-center justify-content-center">
-                            <div
-                              className="mx-auto"
-                              style={{ maxWidth: "150px", cursor: "pointer" }}
-                            >
-                              <a href={`${val.url}`}>
-                                <div
-                                  className="p-2"
-                                  style={{
-                                    backgroundColor: "#000000",
-                                    color: "#ffffff",
-                                    cursor: "pointer",
-                                    borderRadius: "20px",
-                                  }}
-                                >
-                                  <a>APPLY NOW</a>
-                                </div>
-                              </a>
-                            </div>
-                          </Col>
-                        </div>
-                      );
-                    } else {
-                      return (
-                        <div
-                          style={{
-                            backgroundColor: "rgba(217, 217, 217, 0.28)",
-                          }}
-                          className="p-3 d-flex align-items-center"
-                        >
-                          <Col>{`${val.job_title}`}</Col>
-                          <Col className="text-center">{`${val.type_job}`}</Col>
-                          <Col className="text-center">{`${val.city}`}</Col>
-                          <Col className="text-center justify-content-center">
-                            <div
-                              className="mx-auto"
-                              style={{ maxWidth: "150px", cursor: "pointer" }}
-                            >
-                              <a href={`${val.url}`}>
-                                <div
-                                  className="p-2"
-                                  style={{
-                                    backgroundColor: "#000000",
-                                    color: "#ffffff",
-                                    cursor: "pointer",
-                                    borderRadius: "20px",
-                                  }}
-                                >
-                                  <a>APPLY NOW</a>
-                                </div>
-                              </a>
-                            </div>
-                          </Col>
-                        </div>
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-            </Container>
-          </div>
-        </div>
+  return (
+    <>
+      <div>
         <div style={{ backgroundColor: "#000000", height: "1px" }}></div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div>
-          <div style={{ backgroundColor: "#000000", height: "1px" }}></div>
-          <div
-            className="py-4"
-            style={{
-              width: "100%",
-              // borderTop: "2px solid #000000",
-            }}
-          >
-            <Container>
-              <div
-                className="d-flex align-items-center justify-content-between"
-                {...getToggleProps()}
-              >
-                {isExpanded ? (
-                  <>
-                    <div>
-                      <h2
-                        style={{
-                          color: "#000000",
-                          fontStyle: "bold",
-                        }}
-                      >
-                        {`${title_departement}`}
-                      </h2>
+        <div
+          className="py-4"
+          style={{
+            width: "100%",
+            // borderTop: "2px solid #000000",
+          }}
+        >
+          <Container>
+            <div
+              className="d-flex align-items-center justify-content-between"
+              {...getToggleProps()}
+            >
+              {isExpanded ? (
+                <>
+                  <div>
+                    <h2
+                      style={{
+                        color: "#000000",
+                        fontStyle: "bold",
+                      }}
+                    >
+                      {`${title_departement}`}
+                    </h2>
+                    <div
+                      style={{ color: "#7B7B7B" }}
+                    >{`${length} OPEN ROLES`}</div>
+                  </div>
+                  <div>
+                    <img
+                      src={require("assets/img/icons/common/up_vector.png")}
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div>
+                    <h2
+                      style={{
+                        color: "#000000",
+                        fontStyle: "bold",
+                      }}
+                    >
+                      {`${title_departement}`}
+                    </h2>
+                    <div
+                      style={{ color: "#7B7B7B" }}
+                    >{`${length} OPEN ROLES`}</div>
+                  </div>
+                  <div>
+                    <img
+                      src={require("assets/img/icons/common/down_vector.png")}
+                    />
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="mt-4" {...getCollapseProps()}>
+              <div>{`${description_departement}`}</div>
+              <div className="mt-4">
+                {job.map((val, index) => {
+                  if (index % 2 == 0) {
+                    return (
                       <div
-                        style={{ color: "#7B7B7B" }}
-                      >{`${length} OPEN ROLES`}</div>
-                    </div>
-                    <div>
-                      <img
-                        src={require("assets/img/icons/common/up_vector.png")}
-                      />
-                    </div>
-                  </>
-                ) : (
-                  <>
-                    <div>
-                      <h2
-                        style={{
-                          color: "#000000",
-                          fontStyle: "bold",
-                        }}
+                        style={{ backgroundColor: "#D9D9D9CC" }}
+                        className="p-3 d-flex align-items-center"
                       >
-                        {`${title_departement}`}
-                      </h2>
+                        <Col>{`${val.job_title}`}</Col>
+                        <Col className="text-center">{`${val.type_job}`}</Col>
+                        <Col className="text-center">{`${val.city}`}</Col>
+                        <Col className="text-center justify-content-center">
+                          <div
+                            className="mx-auto"
+                            style={{ maxWidth: "150px", cursor: "pointer" }}
+                          >
+                            <a href={`${val.url}`}>
+                              <div
+                                className="p-2"
+                                style={{
+                                  backgroundColor: "#000000",
+                                  color: "#ffffff",
+                                  cursor: "pointer",
+                                  borderRadius: "20px",
+                                }}
+                              >
+                                <a>APPLY NOW</a>
+                              </div>
+                            </a>
+                          </div>
+                        </Col>
+                      </div>
+                    );
+                  } else {
+                    return (
                       <div
-                        style={{ color: "#7B7B7B" }}
-                      >{`${length} OPEN ROLES`}</div>
-                    </div>
-                    <div>
-                      <img
-                        src={require("assets/img/icons/common/down_vector.png")}
-                      />
-                    </div>
-                  </>
-                )}
+                        style={{ backgroundColor: "rgba(217, 217, 217, 0.28)" }}
+                        className="p-3 d-flex align-items-center"
+                      >
+                        <Col>{`${val.job_title}`}</Col>
+                        <Col className="text-center">{`${val.type_job}`}</Col>
+                        <Col className="text-center">{`${val.city}`}</Col>
+                        <Col className="text-center justify-content-center">
+                          <div
+                            className="mx-auto"
+                            style={{ maxWidth: "150px", cursor: "pointer" }}
+                          >
+                            <a href={`${val.url}`}>
+                              <div
+                                className="p-2"
+                                style={{
+                                  backgroundColor: "#000000",
+                                  color: "#ffffff",
+                                  cursor: "pointer",
+                                  borderRadius: "20px",
+                                }}
+                              >
+                                <a>APPLY NOW</a>
+                              </div>
+                            </a>
+                          </div>
+                        </Col>
+                      </div>
+                    );
+                  }
+                })}
               </div>
-              <div className="mt-4" {...getCollapseProps()}>
-                <div>{`${description_departement}`}</div>
-                <div className="mt-4">
-                  {job.map((val, index) => {
-                    if (index % 2 == 0) {
-                      return (
-                        <div
-                          style={{ backgroundColor: "#D9D9D9CC" }}
-                          className="p-3 d-flex align-items-center"
-                        >
-                          <Col>{`${val.job_title}`}</Col>
-                          <Col className="text-center">{`${val.type_job}`}</Col>
-                          <Col className="text-center">{`${val.city}`}</Col>
-                          <Col className="text-center justify-content-center">
-                            <div
-                              className="mx-auto"
-                              style={{ maxWidth: "150px", cursor: "pointer" }}
-                            >
-                              <a href={`${val.url}`}>
-                                <div
-                                  className="p-2"
-                                  style={{
-                                    backgroundColor: "#000000",
-                                    color: "#ffffff",
-                                    cursor: "pointer",
-                                    borderRadius: "20px",
-                                  }}
-                                >
-                                  <a>APPLY NOW</a>
-                                </div>
-                              </a>
-                            </div>
-                          </Col>
-                        </div>
-                      );
-                    } else {
-                      return (
-                        <div
-                          style={{
-                            backgroundColor: "rgba(217, 217, 217, 0.28)",
-                          }}
-                          className="p-3 d-flex align-items-center"
-                        >
-                          <Col>{`${val.job_title}`}</Col>
-                          <Col className="text-center">{`${val.type_job}`}</Col>
-                          <Col className="text-center">{`${val.city}`}</Col>
-                          <Col className="text-center justify-content-center">
-                            <div
-                              className="mx-auto"
-                              style={{ maxWidth: "150px", cursor: "pointer" }}
-                            >
-                              <a href={`${val.url}`}>
-                                <div
-                                  className="p-2"
-                                  style={{
-                                    backgroundColor: "#000000",
-                                    color: "#ffffff",
-                                    cursor: "pointer",
-                                    borderRadius: "20px",
-                                  }}
-                                >
-                                  <a>APPLY NOW</a>
-                                </div>
-                              </a>
-                            </div>
-                          </Col>
-                        </div>
-                      );
-                    }
-                  })}
-                </div>
-              </div>
-            </Container>
-          </div>
+            </div>
+          </Container>
         </div>
-      </>
-    );
-  }
+      </div>
+      {last == true ? (
+        <>
+          <div style={{ backgroundColor: "#000000", height: "1px" }}></div>
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 
 CardsCareer.propTypes = {
