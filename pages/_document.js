4cbc1c975772035617abe77712367e84e584d048
Wahyu Saefulloh <saefuuloh@gmail.com>
2022-11-28 13:20:30 +0700

    add

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -24,11 +25,52 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
             rel="stylesheet"
           />
+
+          {/* <!-- Meta Pixel Code --> */}
+          <Script id="facebook-pixel">
+            {`
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1283819782458772');
+              fbq('track', 'PageView');
+            `}
+          </Script>
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style="display:none"
+              src="https://www.facebook.com/tr?id=1283819782458772&ev=PageView&noscript=1"
+            />
+          </noscript>
+          {/* <!-- Meta Pixel Code --> */}
         </Head>
         <body className="g-sidenav-show g-sidenav-pinned">
           <div id="page-transition"></div>
           <Main />
           <NextScript />
+
+          {/* <!-- Google tag (gtag.js) --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-250295961-1"
+          ></script>
+          <Script id="google-tag-manager">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+      
+              gtag('config', 'UA-250295961-1');
+            `}
+          </Script>
+          {/* <!-- Google tag (gtag.js) --> */}
         </body>
       </Html>
     );
